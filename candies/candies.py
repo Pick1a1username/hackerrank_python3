d3b0d514a53215f537f76692f7e8efd6a10c07b1
Pick1a1username <xscreensaver@outlook.com>
2023-08-15 08:54:34 +0900

    20230815_0

--- candies/candies.py
+++ candies/candies.py
@@ -20,20 +20,28 @@ def candies(n, arr):
     for i in range(1,n+1):
         if arr[i-1] >= arr[i] <= arr[i+1]:
             candies[i] = 1
+    save_candies(candies, 'after_valleys.txt')
 
     # populate 'rises'
     for i in range(1,n+1):
         if arr[i-1] < arr[i] <= arr[i+1]:
             candies[i] = candies[i-1] + 1
+    save_candies(candies, 'after_rises.txt')
 
     # populate 'falls'
     for i in range(n,0,-1):
         if arr[i-1] >= arr[i] > arr[i+1]:
             candies[i] = candies[i+1] + 1
+    save_candies(candies, 'after_falls.txt')
 
     # populate 'peaks'
     for i in range(1,n+1):
         if arr[i-1] < arr[i] > arr[i+1]:
             candies[i] = max(candies[i-1], candies[i+1]) + 1
+    save_candies(candies, 'after_peaks.txt')
 
     return sum(candies)
+
+def save_candies(cadies, save_path):
+    with open(save_path, 'w') as f:
+        f.write('\n'.join([str(x) for x in cadies]))
